c4890a4feb811f94e69fdefde29073a47c9e9737
mutuam <lodekib@gmail.com>
2023-11-23 13:19:25 +0300

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,33 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');  // Fix the typo here
+    cartitem.classList.remove('active');
+}
+
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartitem.classList.remove('active');
+}
+
+
+
+let cartitem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartitem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchFprm.classList.remove('active');
+}
+
+Window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active'); 
+    cartitem.classList.remove('active');
+   
+}
